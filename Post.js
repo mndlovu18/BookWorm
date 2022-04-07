9cba2ab5a4174c63f609d6f01b30d275cfa7c036
Minolta Ndlovu <62041069+mndlovu18@users.noreply.github.com>
2022-04-07 16:19:21 -0400

    added create and view posts endpoints

--- Post.js
+++ Post.js
@@ -1,13 +1,16 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/posts");
+mongoose.connect("mongodb://localhost:27017/posts");
 var Schema = mongoose.Schema;
 
 var postSchema = new Schema({
-  title: String,
-  content: String,
+  title: { type: String, required: true },
   name: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  content: { type: String, required: true },
+  //assign a timestamp to each post
+  created: { type: Date, default: Date.now },
+  //assign an id to each post
+  _id: { type: Number, required: true },
 });
 
 //export the Post class
--- index.js
+++ index.js
@@ -9,6 +9,71 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // import the Post class from Post.js
 var Post = require("./Post.js");
 
+/***************************************/
+
+//endpoint for creating a new post
+//this is the action of the "create new post" form
+app.use("/create", (req, res) => {
+  //create the new post
+  var newPost = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    name: req.body.name,
+    //assign a timestamp to each post
+    created: Date.now(),
+    //assign an id to each post
+    _id: Math.floor(Math.random() * 1000000),
+  });
+
+  //save the new post to the database
+  newPost.save((err) => {
+    if (err) {
+      res.type("html").status(200);
+      res.write("uh oh: " + err);
+      console.log(err);
+      res.end();
+    } else {
+      //redirect to the homepage
+      res.redirect("/posts");
+    }
+  });
+});
+
+//endpoint for showing all posts
+app.use("/posts", (req, res) => {
+  //find all posts in the database
+  Post.find({}, (err, posts) => {
+    if (err) {
+      res.type("html").status(200);
+      res.write("uh oh: " + err);
+      res.write(err);
+    } else {
+      if (posts.length === 0) {
+        res.type("html").status(200);
+        res.write("There are no posts in the database");
+        res.end();
+        return;
+      } else {
+        res.type("html").status(200);
+        res.write("Here are all the posts in the database: ");
+        res.write("<ul>");
+        //show each post in the database
+        posts.forEach((post) => {
+          res.write("<h3>" + post.title + "</h3>");
+          res.write("<p>" + post.title + "</p>");
+          res.write("<p>" + post.content + "</p>");
+          res.write("<p>" + post.created + "</p>");
+          res.write("<p>" + "id: " + post._id + "</p>");
+          res.write('<a href="/delete?_id=' + post._id + '">delete</a>');
+          res.write("<br>");
+        });
+        res.write("</ul>");
+        res.end();
+      }
+    }
+  });
+});
+
 app.use("/public", express.static("public"));
 
 app.use("/", (req, res) => {
